c3adda1d46ef960e6ba452fc05e19f316a27f544
geziliu <59383373+geziliu@users.noreply.github.com>
2024-12-02 01:00:52 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -4,6 +4,13 @@ REDROID="android-redroid8"
 SCRCPY="android-scrcpy-web"
 NGINX="android-nginx"
 
-docker restart ${REDROID}
-docker restart ${SCRCPY}
-docker restart ${NGINX}
+
+docker start ${REDROID}
+
+docker start ${SCRCPY}
+
+sleep 8
+echo -e "\n 3.${SCRCPY} adb connect ${REDROID}"
+docker exec -it ${SCRCPY} adb connect ${REDROID}:5555
+
+docker start ${NGINX}
